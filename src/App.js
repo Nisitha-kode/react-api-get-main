aad48cb3f974f1a1ba46b310273b4c57a6ac8732
NisithaPrakash <nisitha.kodehode@gmail.com>
2022-11-21 11:11:53 +0100

    Added UseContext

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg'
+import './App.css'
+import Activities from './components/Activites'
+import UseContext from './components/UseContext'
+
+function App () {
+  return (
+    <>
+      <Activities />
+
+      <UseContext />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Child.jsx
@@ -0,0 +1,13 @@
+const Child = props => {
+  const { data } = props
+
+  return (
+    <>
+      <h1>Child</h1>
+
+      <h1>{data.name}</h1>
+    </>
+  )
+}
+
+export default Child
