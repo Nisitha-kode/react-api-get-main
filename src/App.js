b73072354c52252cc0845ce48590c7a7d1ce8938
NisithaPrakash <nisitha.kodehode@gmail.com>
2022-11-22 13:48:38 +0100

    Made some changes in the code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 //import logo from './logo.svg'
 import './App.css'
+import {BrowserRouter as Router,Route,Link } from "react-router-dom";
 import Activities from './components/Activites'
-import UseContext from './components/UseContext'
+import UseContext from './components/UserContext'
+import {Index} from "./components/";
+import {About} from  "./components/about";
 
-function App () {
+function AppRouter () {
+  const[]
   return (
-    <>
-      <Activities />
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/Activities/">Activities</Link>
+            </li>
+          </ul>
+        </nav>
+      <UserContext.Provider value={{activities,setActivities}}>
+        <Route path="/" exact component={Index}/>
+        <Route path="/about" exact component={About}/>
+        <Route path="/Activities" exact component={Activities}/>
+      </UserContext.Provider>
 
-      <UseContext />
-    </>
+         </div>
+    </Router>
+
+    
   )
 }
 
-export default App
+export default AppRouter
---
+++ src/components/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+export function About() 
+
+   const{activities,setActivities}=useContext{UserContext}
+    return 
+    <div>
+    <h2>About</h2>
+   <div>{value}</div>
+   <button onClick={()} => setActivities()>change value</button>
+   </div>
